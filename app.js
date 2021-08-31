48bd0856ab60ff07cdc4a5377ae3b0af8cc6e6ef
Atiyah Ellerbee <atiyahbellerbee@gmail.com>
2021-08-31 19:39:30 -0400

    working tables

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize(
     process.env.DB_DBNAME,
     process.env.DB_USER,
@@ -11,12 +11,51 @@ const sequelize = new Sequelize(
     }
 );
 
+
+const User = sequelize.define("User", {
+    username: {
+        type:DataTypes.STRING
+    }
+})
+/** 
+    * One To One Rel 
+ */
+const Profile = sequelize.define("Profile", {
+    birthday: {
+        type: DataTypes.DATE
+    }
+})
+
+User.hasOne(Profile, {
+    onDelete: "CASCADE"
+});
+Profile.belongsTo(User);
+/** 
+    * One To Many
+ */
+const Order = sequelize.define("Order", {
+    shipDate: {
+        type: DataTypes.DATE
+    }
+})
+User.hasMany(Order);
+Order.belongsTo(User);
+/** 
+    * Many To Many
+ */
+const Class = sequelize.define("Class", {
+    className: {
+        type: DataTypes.STRING
+    },
+    startDate: {
+        type: DataTypes.DATE
+    }
+})
+
+User.belongsToMany(Class, { through: "Users_Classes" })
+Class.belongsToMany(User, { through: "Users_Classes" })
+
 //iffie
-(( async() => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-  } catch (error) {
-        console.error('Unable to connect to the database:', error);
-  }
-})());
+; (async () => {
+    await sequelize.sync({force: true});
+})();
